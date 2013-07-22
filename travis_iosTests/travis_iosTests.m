5f7faa9c4c73703b2d03be8cd0bcf2e989bf7270
Thomas Gautier <thomas.gautier@gmail.com>
2013-07-22 20:42:37 +0200

    Add Travis with a passing test to check that it works

--- travis_iosTests/travis_iosTests.m
+++ travis_iosTests/travis_iosTests.m
@@ -17,20 +17,16 @@
 - (void)setUp
 {
     [super setUp];
-    
-    // Set-up code here.
 }
 
 - (void)tearDown
 {
-    // Tear-down code here.
-    
     [super tearDown];
 }
 
-- (void)testExample
+- (void)shouldBeTrue
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    XCTAssertTrue(TRUE);
 }
 
 @end
